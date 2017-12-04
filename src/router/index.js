d905decc2ba84c667a9e687ccc292937b44e9c5f
littlezong <921674302@qq.com>
2017-12-04 13:48:39 +0800

    gnoz 1204 :book:

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-
+// import DeskTop from '@/views/DeskTop';
+// import Music from '@/views/Music';
 import Blog from '@/views/Blog';
 import PostList from '@/views/Blog/PostList';
 import Post from '@/views/Blog/Post';
+import MarkdownEditor from '@/views/MarkdownEditor';
 import About from '@/views/About';
 
 Vue.use(Router);
@@ -19,7 +21,7 @@ export default new Router({
                 name: 'PostList',
                 component: PostList
             },{
-                path: 'post/:target',
+                path: 'post/:title/:target/:date/:tags',
                 name: 'Post',
                 component: Post
             },{
@@ -27,6 +29,33 @@ export default new Router({
                 name: 'About',
                 component: About
             }]
+        },
+        {
+            path: '/markdown',
+            name: 'MarkdownEditor',
+            component: MarkdownEditor
         }
+        // {
+        //     path: '/',
+        //     name: 'DeskTop',
+        //     component: DeskTop
+        // },{
+        //     path: '/music',
+        //     name: 'Music',
+        //     component: Music
+        // }, {
+        //     path: '/blog',
+        //     name: 'Blog',
+        //     component: Blog,
+        //     children: [{
+        //         path: '',
+        //         name: 'PostList',
+        //         component: PostList
+        //     },{
+        //         path: 'post/:target',
+        //         name: 'Post',
+        //         component: Post
+        //     }]
+        // }
     ]
 })
